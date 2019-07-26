928c78be3281a1237ee1c4b21ac89b5d633d3bed
Andrew Richardson <andrewlowellr@gmail.com>
2019-07-26 10:40:18 -0600

    Ge tup to date

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,22 +1,23 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    # elements = len( arrA ) + len( arrB )
+    merged_arr = []
     # TO-DO
-
-    if elements == 0:
-      merged_arr = []
-    elif len(arrA) == 0:
-      merged_arr = arrB
-    elif len(arrB) == 0:
-      merged_arr = arrA
-    else:
-      if arrA[0] < arrB[0]:
-        merged_arr = [arrA[0]] + merge(arrA[1:], arrB)
-      else:
-        merged_arr = [arrB[0]] + merge(arrB[1:], arrA)
-
     
+    i, j = 0, 0
+    while i < len(arrA) or j < len(arrB):
+      if i == len(arrA):
+        merged_arr = merged_arr + arrB[j:]
+        break
+      elif j == len(arrB):
+        merged_arr = merged_arr + arrA[i:]
+        break
+      elif arrA[i] < arrB[j]:
+        merged_arr.append(arrA[i])
+        i += 1
+      else:
+        merged_arr.append(arrB[j])
+        j += 1    
+      
     return merged_arr
 
 # print(merge([2, 3, 5, 7, 9, 12, 45, 67, 89], [4, 6, 8, 13, 16, 26, 35]))
@@ -24,14 +25,17 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) <= 1:
+      return arr
 
-    if len(arr) > 1:
-      div = len(arr) // 2
-      arr = merge(merge_sort(arr[:div]), merge_sort(arr[div:]))
+    pivot = len(arr) // 2
+    left = merge_sort(arr[:pivot])
+    right = merge_sort(arr[pivot:])
 
-    return arr
+    return merge(left, right)
 
-# print(merge_sort([2, 5, 3, 8, 5, 8, 23, 67, 43, 89, 234, 567, 234, 12, 67, 34, 6, 3, 1]))
+print(merge_sort([2, 3, 554, 7, 9, 12, 45, 674, 89, 4, 6, 8, 13, 16, 26, 35]))
+print(merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
